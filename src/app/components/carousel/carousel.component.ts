2f2d1ba6f3e554d35b212df17ce5087458e6178a
Sathya narayanan <sathya9044@gmail.com>
2020-06-17 08:13:20 +0530

    Removed Carousel view

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { CarouselConfig } from 'ngx-bootstrap/carousel';
 @Component({
   selector: 'sharp-carousel',
   templateUrl: './carousel.component.html',
